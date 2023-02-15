b075c650639d44a8f6b446ed20f8dac3d7310dde
DelioAraujo <delioaraujo@gmail.com>
2023-02-15 15:19:20 -0300

    itens de 1 a 7 - tentativa 01

---
+++ script.js
@@ -0,0 +1,17 @@
+const login = ()=>{
+    const email = document.querySelector("#email")
+const senha = document.querySelector("#senha")
+const entrar = document.querySelector("#btn-entrar")
+
+
+entrar.addEventListener("click", ()=>{
+    if(email.value==="trybe@teste.com" && senha.value==="123456"){
+        alert("Olá, Tryber!")
+    } else {
+        alert("Email ou senha inválidos")
+    } 
+})
+
+}
+login();
+
